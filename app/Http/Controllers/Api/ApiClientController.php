2d33e48e16dae881664056728de561d1b788260a
tsion beyene <tsibeyeu@gmail.com>
2025-08-09 13:00:14 -0700

    change on controller

--- app/Http/Controllers/Api/ApiClientController.php
+++ app/Http/Controllers/Api/ApiClientController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Client;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Hash;
 
 class ApiClientController extends Controller
 {
